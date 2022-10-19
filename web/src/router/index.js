1df761ce3fec961c8aab55727555d7bb0bae3f3c
lhy <2722562862@qq.com>
2022-10-19 09:26:05 +0800

    创建地图、导航栏

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import PKIndexView from '../views/pk/PKIndexView.vue'
+import RecordIndexView from '../views/record/RecordIndexView.vue'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView.vue'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView.vue'
+import NotFoundIndexView from '../views/error/NotFound.vue'
 
 const routes = [
+  {  //设置首页路径为pk路径
+    path: "/",
+    name: "home",
+    redirect: "/pk/"
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PKIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFoundIndexView,
+  },
+  {  //输入其他错误路径会跳转到404界面
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
+  }
+  
 ]
 
 const router = createRouter({
---
+++ web/src/views/pk/PKIndexView.vue
@@ -0,0 +1,42 @@
+<template>
+    <PlatGround />
+</template>
+
+<script>
+import PlatGround from '../../components/PlayGround.vue'
+
+export default {
+    components: {
+        PlatGround
+    }
+}
+</script>
+
+<style scoped>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</style>
